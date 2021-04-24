fffcc58f166a7c9228c4de0cea8b26b80ab84134
ThivyaaMosesGovindarajoo <81457901+ThivyaaMosesGovindarajoo@users.noreply.github.com>
2021-04-24 13:37:40 +0800

    Add files via upload

---
+++ Tutorial 6_Thivyaa.R
@@ -0,0 +1,29 @@
+install.packages("shiny")
+library(shiny)
+
+#Calculator for a Surface Area of a Sphere
+ui<- pageWithSidebar(
+  #Application title
+  headerPanel("Surface Area of a Sphere"),
+  sidebarPanel(
+    numericInput('r', 'in cm',20, min = 1, max = 200,step = 0.1
+  ),
+  submitButton('Submit')
+  ),
+  mainPanel(
+    h3('The surface area of the sphere is'),
+    h4('The radius entered in cm'),
+    verbatimTextOutput("inputValuer"),
+    h4('The result of the surface area is :'),
+    verbatimTextOutput("Area")
+  )
+)
+  
+Area<- function(r) 4*pi*(r^2)
+
+server<- function(input, output) {
+  output$inputValuer <- renderPrint({input$r})
+  output$Area<- renderPrint({Area(input$r)})
+}
+
+shinyApp(ui = ui, server = server)
